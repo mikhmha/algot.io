f9953e7015185f1e9d2dc168800a39da4f097a33
mikhmha <mikhahmed@protonmail.com>
2021-02-25 18:08:35 -0700

    add info to info

--- pages/info.js
+++ pages/info.js
@@ -11,26 +11,28 @@ export default function Info() {
         </Head>
 
         <p>
-          This website provides live visualization of a high-frequency-trading algorithim I wrote in C++. 
-          The algorithim runs on my local machine while also logging transactions asynchronously to the cloud. 
+          This website provides live visualization of a high-frequency-trading algorithm I wrote in C++. 
+          The algorithm runs on my local machine while also logging transactions asynchronously to the cloud. 
         </p>
         <p>
-           Currently this website is testing, so you may see data feeds being wiped occasionally. 
+           Currently this website is testing so you may see data feeds being wiped occasionally. 
         </p>
         <p>
-          The assets currently being traded are not real but they are traded on
+          The assets being traded are simulated but they are traded on
           real real-time data. Currently only cryptocurrencies are "traded" due to the high cost of obtaining
-          real-time stock data.
+          real-time stock data. The development framework is pretty flexible so it can be pointed to a variety of crypto data feeds. If theres a cryptocurrency you would like to see traded send a suggestion! 
+          Maybe we can try trading it for a few days.
         </p>
         <p>
-          Exchange fees are currently not factored into the descion to make trades. So displayed results not accurate. 
+          Exchange fees are currently not factored into the decision to make trades. So displayed results not accurate. 
         </p>
 
         <p>
-          There are some user facing features planned for this site. Eventually users will be able to influence descions made by the high-frequency trading algorithim directly and indirectly.
+          There are some user facing features planned for this site. Eventually users will be able to influence decision made by the high-frequency trading algorithim directly and indirectly.
         </p>
         <div></div>
-
+        <h1>contact </h1>
+        <a href="https://github.com/mikhmha/algot.io">github</a>
 
         </div>
     )
