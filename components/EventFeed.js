63405cc407ac7f78dad4fe27be9a7506d06ae9cb
aun <mikhahmed@protonmail.com>
2021-03-09 21:17:05 -0700

    event addition

--- components/EventFeed.js
+++ components/EventFeed.js
@@ -8,7 +8,20 @@ function PostEvent({ event }) {
 
     return (
       <Fragment>
-        <div style ={{color: 'brown'}}>inactivity</div> <div>set cur bid/ask to market </div> <div style ={{color: event.status === '1' ? 'green' : 'red'}}>execute</div> <div>{new Date(event.processedAt.seconds * 1000).toLocaleString("en-US")} </div> 
+        <div style ={{color: 'brown'}}>inactivity</div> 
+        <div>set cur bid/ask to market </div> 
+        {(() => {
+              switch (event.status) 
+              {
+                case '0':
+                  return  <div style ={{color: "red"}}>  {"reject"} </div>;
+                case '1':
+                  return  <div style ={{color: "green"}}>  {"execute"} </div>;
+                default:
+                  return null;
+               }
+        })()}
+        <div>{new Date(event.processedAt.seconds * 1000).toLocaleString("en-US")} </div> 
       </Fragment>
   
  
