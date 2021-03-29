1e392ee087b9aa99bbfde04ec634dd03ca23f237
aun <mikhahmed@protonmail.com>
2021-03-28 23:21:35 -0600

    graph add

--- components/ConStatus.js
+++ components/ConStatus.js
@@ -5,17 +5,17 @@ export default function  ConStatus({status}) {
         <div>
             <h3>hft client connection status</h3>
             {(() => {
-        switch (status) {
-          case '0':
-            return  <div style ={{color: "red"}}>  {"offline - maintainence"} </div>;
-          case '1':
-            return  <div style ={{color: "green"}}>  {"online - trading"} </div>;
-          case '2':
-            return  <div style ={{color: "orange"}}>  {"online - testing"} </div>;
-          default:
-            return null;
-        }
-      })()}
+              switch (status) {
+                case '0':
+                  return  <div style ={{color: "red"}}>  {"offline - maintainence"} </div>;
+                case '1':
+                  return  <div style ={{color: "green"}}>  {"online - trading"} </div>;
+                case '2':
+                  return  <div style ={{color: "orange"}}>  {"online - testing"} </div>;
+                default:
+                  return null;
+              }
+            })()}
         </div>
 
     );
